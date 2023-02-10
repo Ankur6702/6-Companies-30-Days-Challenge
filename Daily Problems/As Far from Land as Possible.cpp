f070bf7649938d8d90bb9a81026cabc42a85ab51
Ankur <ankuragarwa246@gmail.com>
2023-02-10 09:56:11 +0530

    Ques solved

---
+++ Daily Problems/As Far from Land as Possible.cpp
@@ -0,0 +1,43 @@
+// As Far from Land as Possible
+// Link: https://leetcode.com/problems/as-far-from-land-as-possible/
+// Time Complexity: O(N^2) because we need to traverse the grid.
+// Space Complexity: O(N^2) because we need to store the visited grid.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int n = grid.size();
+        queue<pair<int, int>> q;
+        vector<vector<int>> visited = grid;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j] == 1) 
+                    q.push({i, j});
+            }
+        }
+        if (q.empty() || q.size() == n * n) 
+            return -1;
+        int distance = -1;
+        // visit all four direction i.e. right, left, up, down
+        vector<pair<int, int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (auto [dx, dy] : dirs) {
+                    int i = x + dx, j = y + dy;
+                    if (i >= 0 && i < n && j >= 0 && j < n && visited[i][j] == 0) {
+                        visited[i][j] = 1;
+                        q.push({i, j});
+                    }
+                }
+            }
+            distance++;
+        }
+        return distance;
+    }
+};
